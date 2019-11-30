f00f4666b12be96749e2c35062523389dbc980e4
Leonardo Morais <leonardomorais89@yahoo.com.br>
2019-11-30 14:09:33 -0300

    Busca de usuarios por id funcionando

--- src/main/java/com/example/mongo/resources/UserResource.java
+++ src/main/java/com/example/mongo/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +23,11 @@ public class UserResource {
 	public ResponseEntity<List<User>> findAll() {
 		 List<User> users = service.findAll();
 		 return ResponseEntity.ok().body(users);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
--- src/main/java/com/example/mongo/services/UserService.java
+++ src/main/java/com/example/mongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.example.mongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.get();
 	}
 }
