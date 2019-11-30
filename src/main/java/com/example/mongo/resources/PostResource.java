cfc372527e72404b513e89e66ddcca76b71e0f10
Leonardo Morais <leonardomorais89@yahoo.com.br>
2019-11-30 16:40:33 -0300

    Insert de post funcionando

--- src/main/java/com/example/mongo/resources/PostResource.java
+++ src/main/java/com/example/mongo/resources/PostResource.java
@@ -1,11 +1,16 @@
 package com.example.mongo.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.mongo.entity.Post;
 import com.example.mongo.services.PostService;
@@ -22,4 +27,16 @@ public class PostResource {
 		Post post = service.findById(id);
 		return ResponseEntity.ok().body(post);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Post> insert(@RequestBody Post post) {
+		post = service.insert(post);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(post.getId()).toUri();
+		return ResponseEntity.created(uri).body(post);
+	}
+	
+//	@PutMapping(value = "/{id}")
+//	public ResponseEntity<Post> update(@PathVariable String id, @RequestBody Post post) {
+//		
+//	}
 }
--- src/main/java/com/example/mongo/services/PostService.java
+++ src/main/java/com/example/mongo/services/PostService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.mongo.entity.Post;
+import com.example.mongo.entity.User;
 import com.example.mongo.repository.PostRepository;
+import com.example.mongo.repository.UserRepository;
 
 @Service
 public class PostService {
@@ -14,8 +16,25 @@ public class PostService {
 	@Autowired
 	private PostRepository repo;
 	
+	@Autowired
+	private UserRepository userRepo;
+	
 	public Post findById(String id) {
-		Optional<Post> post = repo.findById(id);
-		return post.get();
+		return repo.findById(id).get();
+	}
+	
+	public Post insert(Post post) {
+		post = repo.insert(post);
+		User user = userRepo.findById(post.getAuthor().getId()).get();
+		user.getPosts().add(post);
+		userRepo.save(user);
+		return post;
+	}
+	
+	public Post update(Post post) {
+		Post newPost = findById(post.getId());
+		newPost.setTitle(post.getTitle());
+		newPost.setBody(post.getBody());
+		return repo.save(newPost);
 	}
 }
